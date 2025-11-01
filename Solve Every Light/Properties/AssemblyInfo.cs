c60ebba14b0f82db22b68b58a6b1987609c320f2
Alexander Wrede <alexander.wrede@gmx.de>
2025-11-01 18:45:58 +0100

    improved

--- Solve Every Light/Properties/AssemblyInfo.cs
+++ Solve Every Light/Properties/AssemblyInfo.cs
@@ -31,18 +31,18 @@ using System.Runtime.InteropServices;
 // The url to the license
 [assembly: AssemblyMetadata("LicenseURL", "https://www.mozilla.org/en-US/MPL/2.0/")]
 // The repository where your plugin is hosted
-[assembly: AssemblyMetadata("Repository", "repourl")]
+[assembly: AssemblyMetadata("Repository", "https://github.com/astroalex80/NINA.Plugin.SolveEveryLight")]
 
 // The following attributes are optional for the official manifest meta data
 
 //[Optional] Your plugin homepage URL - omit if not applicaple
-[assembly: AssemblyMetadata("Homepage", "weburl")]
+[assembly: AssemblyMetadata("Homepage", "")]
 
 //[Optional] Common tags that quickly describe your plugin
-[assembly: AssemblyMetadata("Tags", "")]
+[assembly: AssemblyMetadata("Tags", "plate solving, WCS, astrometry")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
-[assembly: AssemblyMetadata("ChangelogURL", "repourl/CHANGELOG.md")]
+[assembly: AssemblyMetadata("ChangelogURL", "")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
 [assembly: AssemblyMetadata("FeaturedImageURL", "")]
